4306f6868f934703d7b15ed6e31e98984bc5c049
kazwtto <77172419+kazwtto@users.noreply.github.com>
2023-10-11 18:47:36 -0300

    Add files via upload

---
+++ animes.js
@@ -0,0 +1,39 @@
+var syns = (s) => s.split(" ").slice(0, 15).join(" ") + "..."
+
+async function _animesLoad(dom) {
+    var animes = await fetch('animes.json')
+        animes = await animes.json()
+        animes = animes.filter(anime => anime.name_jp.length)
+    
+    for (var anime of animes) {
+        var table       = document.createElement('table');
+        table.className = "animeTable";
+
+        
+        table.innerHTML = `
+        <tr>
+            <td class="img"><img src="${anime.poster}"></td> 
+            <td class="info">
+                <div class="name">
+                  <h2>${anime.name_jp}</h2>
+                  <l>${anime.name_en}</l>
+                </div>
+                <div class="gender">
+                    ${anime.gender.map(l => `<l>${l}</l>`)
+                      .join("\n")}
+                </div>
+                <div class="synopsis">
+                    <l>${syns(anime.synopsis)}</l>
+                </div>
+            
+                <button onclick='window.location.href = "${anime.anime_fire}";' class="watch">
+                    ASSISTIR
+                </button>
+            </td>
+        </tr>
+        
+        `
+        
+        dom.appendChild(table)
+    }
+}
